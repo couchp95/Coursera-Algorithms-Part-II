8256ac13c5d50f33cc0a65d8c0b36f3753a56a09
couchp95 <couchp95@gmail.com>
2022-01-13 11:04:04 +0800

    updated for memory test

--- wk3_Baseball_Elimination/BaseballElimination.java
+++ wk3_Baseball_Elimination/BaseballElimination.java
@@ -5,22 +5,19 @@ import edu.princeton.cs.algs4.FordFulkerson;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class BaseballElimination {
-    private final int num, t;
-    private int s;
+    private final int num;
     private final Map<String, Integer> teams;
-    private final String[] teambyid;
     private final int[] w;
     private final int[] l;
     private final int[] r;
     private final int[][] g;
-    private FlowNetwork flow;
-    private FordFulkerson ff;
-    private int leadingteam;
-    private final boolean[] trivials;
+
+    private final ArrayList<Bag<String>> subset;
 
     // create a baseball division from given filename in format specified below
     public BaseballElimination(String filename) {
@@ -31,12 +28,12 @@ public class BaseballElimination {
         l = new int[num];
         r = new int[num];
         g = new int[num][num];
-        trivials = new boolean[num];
-        teambyid = new String[num];
-        leadingteam = -1;
-        t = num + num * (num - 1) / 2 + 1;
+        String[] teambyid = new String[num];
+        int leadingteam = -1;
+        subset = new ArrayList<Bag<String>>();
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < num; i++) {
+            subset.add(null);
             teambyid[i] = file.readString();
             teams.put(teambyid[i], i);
             w[i] = file.readInt();
@@ -49,8 +46,45 @@ public class BaseballElimination {
                 leadingteam = i;
             }
         }
-        for (int i = 0; i < num; i++)
-            if (w[leadingteam] > w[i] + r[i]) trivials[i] = true;
+        for (int current = 0; current < num; current++)
+            if (w[leadingteam] > w[current] + r[current]) {
+                Bag<String> b = new Bag<String>();
+                for (int j = 0; j < num; j++)
+                    if (w[j] > w[current] + r[current]) b.add(teambyid[j]);
+                subset.add(current, b);
+            }
+            else {
+                int required = minwins(current);
+                FlowNetwork flow = new FlowNetwork(num + (num - 2) * (num - 1) / 2 + 1);
+                // game vertices =n...n+(n-2)*(n-1)/2-1 ; team vertices = 0..n-1
+                // s = x t = num + (num - 2) * (num - 1) / 2
+                int s = current;
+                int t = num + (num - 2) * (num - 1) / 2;
+                int n = num;
+                for (int i = 0; i < num; i++) {
+                    if (i == s) continue;
+                    // create team vertices to t
+                    flow.addEdge(new FlowEdge(i, t, w[s] + r[s] - w[i]));
+                    for (int j = i + 1; j < num; j++) {
+                        if (j == s) continue;
+                        // create s to game vertices
+                        flow.addEdge(new FlowEdge(s, n, g[i][j]));
+                        // create game vertices to team vertices
+                        flow.addEdge(new FlowEdge(n, i, Double.POSITIVE_INFINITY));
+                        flow.addEdge(new FlowEdge(n, j, Double.POSITIVE_INFINITY));
+                        n++;
+                    }
+                }
+                FordFulkerson ff = new FordFulkerson(flow, current, t);
+                if (ff.value() < required) {
+                    Bag<String> b = new Bag<String>();
+                    for (int i = 0; i < num; i++)
+                        if (i != current && ff.inCut(i)) b.add(teambyid[i]);
+                    // for (String f : teams.keySet())
+                    //    if (!f.equals(teambyid[current]) && ff.inCut(teams.get(f))) b.add(f);
+                    subset.add(current, b);
+                }
+            }
     }
 
     // number of teams
@@ -88,8 +122,8 @@ public class BaseballElimination {
         return g[teams.get(team1)][teams.get(team2)];
     }
 
-    private int minwins(String team) {
-        int x = teams.get(team);
+    private int minwins(int x) {
+        // int x = teams.get(team);
         int min = 0;
         for (int i = 0; i < num; i++) {
             if (i == x) continue;
@@ -101,58 +135,19 @@ public class BaseballElimination {
         return min;
     }
 
-    private boolean trivalEliminated(String team) {
-        if (!teams.containsKey(team)) throw new IllegalArgumentException();
-        return trivials[teams.get(team)];
-    }
-
     // is given team eliminated?
     public boolean isEliminated(String team) {
         if (!teams.containsKey(team)) throw new IllegalArgumentException();
-        if (trivalEliminated(team)) return true;
-        int required = minwins(team);
-        buildflownetwork(team);
-        ff = new FordFulkerson(flow, teams.get(team), t);
-        return (ff.value() < required);
+        return (subset.get(teams.get(team)) != null);
     }
 
     // subset R of teams that eliminates given team; null if not eliminated
     public Iterable<String> certificateOfElimination(String team) {
         if (!isEliminated(team)) return null;
-        Bag<String> b = new Bag<String>();
         if (!teams.containsKey(team)) throw new IllegalArgumentException();
-        if (trivalEliminated(team)) {
-            for (int i = 0; i < num; i++)
-                if (w[i] > w[teams.get(team)] + r[teams.get(team)]) b.add(teambyid[i]);
-            return b;
-        }
-        for (String f : teams.keySet())
-            if (!f.equals(team) && ff.inCut(teams.get(f))) b.add(f);
-        return b;
+        return subset.get(teams.get(team));
     }
 
-    private void buildflownetwork(String team) {
-        int x = teams.get(team);
-        flow = new FlowNetwork(num + num * (num - 1) / 2 + 2);
-        // game vertices =n...n+(n-1)*n/2 ; team vertices = 0..n-1
-        // s = x t = n+(n-1)*n/2+1
-        s = x;
-        int n = num;
-        for (int i = 0; i < num; i++) {
-            if (i == x) continue;
-            // create team vertices to t
-            flow.addEdge(new FlowEdge(i, t, w[x] + r[x] - w[i]));
-            for (int j = i + 1; j < num; j++) {
-                if (j == x) continue;
-                // create s to game vertices
-                flow.addEdge(new FlowEdge(s, n, g[i][j]));
-                // create game vertices to team vertices
-                flow.addEdge(new FlowEdge(n, i, Double.POSITIVE_INFINITY));
-                flow.addEdge(new FlowEdge(n, j, Double.POSITIVE_INFINITY));
-                n++;
-            }
-        }
-    }
 
     public static void main(String[] args) {
         BaseballElimination division = new BaseballElimination(args[0]);
